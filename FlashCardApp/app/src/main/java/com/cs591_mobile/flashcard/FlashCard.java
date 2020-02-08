e7aecdfabc952d57970d6389efd4965d9bad29b7
Cliff <tzcliff@outlook.com>
2020-02-08 17:22:11 -0500

    Finalize Flash Card App.

--- FlashCardApp/app/src/main/java/com/cs591_mobile/flashcard/FlashCard.java
+++ FlashCardApp/app/src/main/java/com/cs591_mobile/flashcard/FlashCard.java
@@ -1,5 +1,7 @@
 package com.cs591_mobile.flashcard;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -15,11 +17,12 @@ public class FlashCard extends AppCompatActivity{
     private TextView txtnum1, txtnum2, txtYourAns, txtInteraction;
     private EditText editAns;
     private Button btnSubmit, btnGenerate;
-    private Integer int1, int2, correctResult, count, correctCount;
+    private Integer int1, int2, correctResult = 0, count = 0, correctCount = 0;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_2);
+        setContentView(R.layout.activity_flashcard);
 
         txtnum1 = (TextView)findViewById(R.id.txtnum1);
         txtnum2 = (TextView)findViewById(R.id.txtnum2);
@@ -28,9 +31,30 @@ public class FlashCard extends AppCompatActivity{
         editAns = (EditText)findViewById(R.id.editAns);
         btnSubmit = (Button)findViewById(R.id.btnSubmit);
         btnGenerate = (Button)findViewById(R.id.btnGenerate);
+        btnSubmit.setEnabled(false);
+    }
 
-        count = 0;
-        correctCount = 0;
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("correctCount", correctCount);
+        outState.putInt("int1", int1);
+        outState.putInt("int2", int2);
+        outState.putInt("correctResult", correctResult);
+        outState.putInt("count", count);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        correctCount = savedInstanceState.getInt("correctCount");
+        int1 = savedInstanceState.getInt("int1");
+        int2 = savedInstanceState.getInt("int2");
+        correctResult = savedInstanceState.getInt("correctResult");
+        count = savedInstanceState.getInt("count");
+
+        txtnum1.setText(String.valueOf(int1));
+        txtnum2.setText("÷ " + String.valueOf(int2));
     }
 
     public void generateMathQuestion(){
@@ -50,23 +74,31 @@ public class FlashCard extends AppCompatActivity{
         switch(view.getId()) {
             case R.id.btnGenerate:
                 generateMathQuestion();
+                Toast.makeText(getApplicationContext(),"10 new questions generated",Toast.LENGTH_LONG).show();
+                btnSubmit.setEnabled(true);
 
                 break;
             case R.id.btnSubmit:
-                if (correctResult == Integer.valueOf(editAns.getText().toString())) {
-                    txtInteraction.setText("You Are Correct!");
-                    correctCount++;
-                    count++;
+                try {
+                    if (correctResult == Integer.valueOf(editAns.getText().toString())) {
+                        txtInteraction.setText("You Are Correct!");
+                        correctCount++;
+                        count++;
+                    } else {
+                        txtInteraction.setText("You Are Wrong, Do Better!");
+                        count++;
+                    }
+                    if (count == 10) {
+                        Toast.makeText(getApplicationContext(), "You have got " + correctCount.toString() + " out of 10", Toast.LENGTH_LONG).show();
+                        count = 0;
+                        correctCount = 0;
+                        btnSubmit.setEnabled(false);
+                    } else {
+                        generateMathQuestion();
+                    }
                 }
-                else{
-                    txtInteraction.setText("You Are Wrong, Do Better!");
-                    count++;
-                }
-                if (count == 10){
-                    Toast.makeText(getApplicationContext(),"You have got " + correctCount.toString() + " out of 10",Toast.LENGTH_LONG).show();
-                }
-                else{
-                    generateMathQuestion();
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"Invalid Input!",Toast.LENGTH_LONG).show();
                 }
 
                 break;
