177d303f5be14b9843b477b93616faf9b49a4614
Cedric Wille <cwille97@gmail.com>
2020-02-20 17:07:47 -0500

    update picture on incorrect letter

--- HW4/W4-P5/app/src/main/java/com/cs591_mobile/hangman/MainActivity.java
+++ HW4/W4-P5/app/src/main/java/com/cs591_mobile/hangman/MainActivity.java
@@ -2,12 +2,15 @@ package com.cs591_mobile.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.cs591_mobile.hangman.models.Game;
 
@@ -19,12 +22,19 @@ public class MainActivity extends AppCompatActivity {
     Game game;
     String guessWord = "";
 
+    ImageView image;
+
     TextView textViewGoal;
+
+    int currentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentImage = 0;
+        image = (ImageView) findViewById(R.id.imgHang);
 
         game = new Game();
         int wordLength = game.getWord().length();
@@ -53,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         else{
-            // todo next pic
+            updateImage(image);
         }
     }
 
@@ -65,6 +75,34 @@ public class MainActivity extends AppCompatActivity {
         Log.i("t", guessWord);
         textView.setText(temp);
         textView.setGravity(Gravity.CENTER);
+    }
+
+    public void updateImage(ImageView image) {
+        currentImage++;
+        switch (currentImage) {
+            case 1:
+                Log.i("Kobe", "image 1");
+                image.setImageResource(R.drawable.hangman1);
+                break;
+            case 2:
+                image.setImageResource(R.drawable.hangman2);
+                break;
+            case 3:
+                image.setImageResource(R.drawable.hangman3);
+                break;
+            case 4:
+                image.setImageResource(R.drawable.hangman4);
+                break;
+            case 5:
+                image.setImageResource(R.drawable.hangman5);
+                break;
+            case 6:
+                image.setImageResource(R.drawable.hangman6);
+                break;
+            default:
+                image.setImageResource(R.drawable.hangman6);
+                break;
+        }
     }
 
 }
