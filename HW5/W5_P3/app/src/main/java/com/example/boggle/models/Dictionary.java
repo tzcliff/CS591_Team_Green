556e0ab2667f774cea3cb513a03ff3555de194b6
Cliff <tzcliff@outlook.com>
2020-02-27 12:42:51 -0500

    Refactoring project and adding dictionary, closes #41

---
+++ HW5/W5_P3/app/src/main/java/com/example/boggle/models/Dictionary.java
@@ -0,0 +1,12 @@
+package com.example.boggle.models;
+
+public class Dictionary {
+    private final String dictionary_url;
+
+    public Dictionary(String dictionary_url) {
+        this.dictionary_url = dictionary_url;
+    }
+    public boolean validWord(String word){
+        return false;
+    }
+}
