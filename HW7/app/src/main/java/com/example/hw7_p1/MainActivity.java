67c6d9a3383a859697a2cd61381316bc93a80e4c
Cedric Wille <cwille97@gmail.com>
2020-03-20 16:47:39 -0400

    progress so far, no call or text functionality. Layout mostly complete.

--- HW7/app/src/main/java/com/example/hw7_p1/MainActivity.java
+++ HW7/app/src/main/java/com/example/hw7_p1/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.hw7_p1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String phoneKey = "phoneyKey"; // key values for shared preferences
+    final String textKey = "textKey";
+    final String preferences = "preferences";
+
+    SharedPreferences sharedPreferences;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext(); // context for Toasts
+        sharedPreferences = getSharedPreferences(preferences, Context.MODE_PRIVATE); // SharedPreferences object to save information
+
+
+    }
+
+    // 1. Show the menu items
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.pref_menu, menu);
+        return true;
+    }
+
+    // 2. Handle menu selections
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.prefMenu) {
+            // load preference activity
+            // 1. declare an intent with the current and next activity
+            Intent myIntent = new Intent(MainActivity.this, PreferencesActivity.class);
+            // 2. start the next activity passing in the intent we just created
+            MainActivity.this.startActivity(myIntent);
+            return true; // return true to tell this function that we handled the menu selection
+        }
+        return super.onOptionsItemSelected(item); // default fallthrough behavior
+    }
+
+    public void phoneCall(View view) {
+        // #TODO: phone call
+    }
+
+    public void textMessage(View view) {
+        // #TODO: text message
     }
 }
--- HW7/app/src/main/java/com/example/hw7_p1/PreferencesActivity.java
+++ HW7/app/src/main/java/com/example/hw7_p1/PreferencesActivity.java
@@ -3,13 +3,70 @@ package com.example.hw7_p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Context;
+import android.widget.Toast;
+import android.content.SharedPreferences;
 
 public class PreferencesActivity extends AppCompatActivity {
+
+    EditText phone; // edit text for phone number for emergency call
+    EditText text; // edit text for phone number for emergency text
+    Context context;
+    final String phoneKey = "phoneyKey"; // key values for shared preferences
+    final String textKey = "textKey";
+    final String preferences = "preferences";
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preferences);
+        setContentView(R.layout.activity_preferences); // don't forget to declare this activity in the manifest!
+
+        phone = findViewById(R.id.phoneEdit);
+        text = findViewById(R.id.textEdit);
+
+        context = getApplicationContext(); // context for Toasts
+        sharedPreferences = getSharedPreferences(preferences, Context.MODE_PRIVATE); // SharedPreferences object to save information
+
+        try { // empty default values so that if no preferences has been saved before then the EditText stays empty; we'll use a try catch just in case though
+            phone.setText(sharedPreferences.getString(phoneKey, ""));
+            text.setText(sharedPreferences.getString(textKey, ""));
+        }
+        catch(Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
+
+
+    }
+
+    public void save(View view) {
+        SharedPreferences.Editor editor = sharedPreferences.edit(); // create an editor object
+        editor.putString(phoneKey, phone.getText().toString()); // put the information, key value pairs
+        editor.putString(textKey, text.getText().toString());
+        editor.commit(); // always commit changes
+
+        // Toast
+        CharSequence text = "Information Saved!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+    }
+
+    public void clear(View view) {
+        // delete visible text
+        phone.setText("");
+        text.setText("");
+
+        // delete preferences
+        SharedPreferences.Editor editor = sharedPreferences.edit(); // create an editor object
+        editor.remove(phoneKey); // remove by key
+        editor.remove(textKey);
+        editor.commit(); // always commit changes
     }
 
 }
