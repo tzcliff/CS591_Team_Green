ec671b8b76e8e48ae487f055b27a571d5b9f78d4
Cedric Wille <cwille97@gmail.com>
2020-03-20 15:26:28 -0400

    started HW7

---
+++ HW7/app/src/main/java/com/example/hw7_p1/PreferencesActivity.java
@@ -0,0 +1,15 @@
+package com.example.hw7_p1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+
+}
